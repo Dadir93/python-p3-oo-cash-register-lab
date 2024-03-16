3806c99af25fadbcb47d3908b59ddc9373a4fbbc
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-03-16 21:18:23 +0300

    done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.last_transaction_amount = 0
+
+    def add_item(self, title, price, quantity=1):
+        for _ in range(quantity):
+            self.items.append(title)
+            self.total += price
+            self.last_transaction_amount = price
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = (self.discount / 100) * self.total
+            self.total -= discount_amount
+        else:
+            pass
+
+    def void_last_transaction(self):
+        if self.items:
+            self.total -= self.last_transaction_amount
+            self.items.pop()
+            self.last_transaction_amount = 0
+        else:
+            pass
+        self.total = max(self.total, 0)
